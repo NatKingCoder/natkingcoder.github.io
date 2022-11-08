eb0f65fbd5c28a65de5ff6ece5bbcd7086b15740
NatKingCoder <111017653+NatKingCoder@users.noreply.github.com>
2022-11-08 12:11:07 -0500

    Create navbar.js

---
+++ assets/js/navbar.js
@@ -0,0 +1,7 @@
+//Generates the Navbar at the top of the page (Requires "<div id="navbar"></div>")
+      $("<a>").attr("href", "/about").text("About").css("left", "5%").appendTo("#navbar");
+      $("<a>").attr("href", "/scratch").text("Scratch").css("left", "20%").appendTo("#navbar");
+      $("<a>").attr("href", "/js").text("JavaScript").css("left", "35%").appendTo("#navbar");
+      $("<a>").attr("href", "/html").text("HTML").css("left", "55%").appendTo("#navbar");
+      $("<a>").attr("href", "/bash").text("BASH").css("left", "70%").appendTo("#navbar");
+      $("<a>").attr("href", "/help").text("Help").css("left", "85%").appendTo("#navbar");
