e601e2325e85f40904a1116c9cda67d8b5a350e5
NatKingCoder <111017653+NatKingCoder@users.noreply.github.com>
2022-11-12 18:26:27 -0500

    Update scratch.js

--- assets/js/scratch.js
+++ assets/js/scratch.js
@@ -2,7 +2,7 @@
       localStorage.setItem("projId", projId);
       window.location.href = "/veiw";
     }
-    $("#rosetheme").on("click", function(){launchProject("732596949")});
-    $("#pong").on("click", function(){launchProject("740262734")});
-    $("#scrollvar").on("click", function(){launchProject("593482431")});
-    $("#va").on("click", function(){launchProject("418011527")});
+    $("#rosetheme").on("click", function(){launchProject("rosetheme")});
+    $("#pong").on("click", function(){launchProject("pong")});
+    $("#scrollvar").on("click", function(){launchProject("scrollvar")});
+    $("#va").on("click", function(){launchProject("va")});
