955924317969c762e572e8ceae2a64145f9e25b4
NatKingCoder <111017653+NatKingCoder@users.noreply.github.com>
2022-11-13 10:07:57 -0500

    Create veiw.js

---
+++ assets/js/veiw.js
@@ -0,0 +1,25 @@
+const rosetheme = {
+  name: "Rosemary's Theme: Noteblock",
+  id: "732596949",
+  pram: "?turbo"
+};
+const pong = {
+  name: "One Sprite Pong",
+  id: "740262734",
+  pram: ""
+};
+const scrollvar = {
+  name: "One Sprite Pong",
+  id: "593482431",
+  pram: ""
+};
+const va = {
+  name: "Virtual Assistant",
+  id: "5418011527",
+  pram: ""
+};
+
+var projId = localStorage.getItem("projId");
+const BuildLink = function (url) {
+  
+}
