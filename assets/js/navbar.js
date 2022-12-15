87a97e5d7d71a516af6eadf8efc27180d2f2ee0e
NatKingCoder <111017653+NatKingCoder@users.noreply.github.com>
2022-12-15 08:25:27 -0500

    Update navbar.js

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,8 +1,7 @@
 //Generates the Navbar at the top of the page (Requires "<div id="navbar"></div>")
-      $("<a>").attr("href", "/").text("Home").css("left", "2%").appendTo("#navbar");
-      $("<a>").attr("href", "/about").text("About").css("left", "15%").appendTo("#navbar");
-      $("<a>").attr("href", "/scratch").text("Scratch").css("left", "30%").appendTo("#navbar");
-      $("<a>").attr("href", "/javascript").text("JavaScript").css("left", "45%").appendTo("#navbar");
-      $("<a>").attr("href", "/html").text("HTML").css("left", "65%").appendTo("#navbar");
-      $("<a>").attr("href", "/bash").text("BASH").css("left", "78%").appendTo("#navbar");
-      $("<a>").attr("href", "/music").text("Music").css("left", "90%").appendTo("#navbar");
+      $("<a>").attr("href", "/").text("Home").css("left", "5%").appendTo("#navbar");
+      $("<a>").attr("href", "/about").text("About").css("left", "20%").appendTo("#navbar");
+      $("<a>").attr("href", "/scratch").text("Scratch").css("left", "35%").appendTo("#navbar");
+      $("<a>").attr("href", "/javascript").text("JavaScript").css("left", "50%").appendTo("#navbar");
+      $("<a>").attr("href", "/html").text("HTML").css("left", "70%").appendTo("#navbar");
+      $("<a>").attr("href", "/bash").text("BASH").css("left", "85%").appendTo("#navbar");
